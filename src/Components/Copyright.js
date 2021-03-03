f7af0e0b4128517207de3682764ffb22756e6c21
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2021-03-03 13:22:44 +0300

    Update Copyright.js

--- src/Components/Copyright.js
+++ src/Components/Copyright.js
@@ -2,12 +2,16 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function Copyright() {
   return (
     <>
       <a href="https://twitter.com/q8fawazo" style={{ color: "#89CFF0" }}>
         <TwitterIcon />
+      </a>
+      <a href="https://github.com/smokeme/revshell" style={{ color: "#ffffff" }}>
+        <GitHubIcon />
       </a>
       <Typography variant="body2" color="textSecondary" align="center">
         {"Copyright © "}
